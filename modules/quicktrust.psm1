89be89632f31950aab8fc5f62e35871eb2c14597
Boredcat <hauntslg@gmailcom>
2025-08-24 00:59:56 +1200

    removed gui in steam and added quicktrust

---
+++ modules/quicktrust.psm1
@@ -0,0 +1,32 @@
+function quicktrust {
+    $modules = $global:pref.ShellModules.Keys
+    $modulesDirectory = Join-Path $global:projectDirectory "modules"
+    $confirm = $false # for warning the user about running powershell code
+
+    # Warn user about running modules, because powershell is dumb and can brick your pc 
+    Write-Host "Are you sure? [Y/N]" -ForegroundColor Cyan
+    Write-Host "Never run powershell code unless you know what it does" -ForegroundColor Yellow
+    Write-Host $global.projectDirectory # am i dumb- # yes, i am dumb
+    Write-Host "This command will enable..." -ForegroundColor Yellow
+    foreach($module in $modules) {
+        Write-Host "                            $module" -ForegroundColor Cyan
+    }
+
+    $userInput = Read-Host
+    
+    if($userInput.ToLower() -eq "y") {
+        $confirm = $true
+    } elseif ($userInput.ToLower() -ne "n") {
+        Write-Host "Invalid response" -ForegroundColor Red
+    }
+
+    # If the user says yes (y) trust every module in the modules directory
+    if($confirm) {
+        foreach($module in $modules) {
+            Write-Host "Trusting $module..." -ForegroundColor Yellow
+            $modulePath = Join-Path $modulesDirectory "$module.psm1"
+            Unblock-File $modulePath
+        }
+        Write-Host "Success" -ForegroundColor Green
+    }
+}
