28d7dfbb706010a796c6eb2757c6a8266c653c1c
Boredcat <hauntslg@gmailcom>
2025-11-09 23:12:58 +1300

    Began rewrite with lazy loading because i thought it would be a good idea (losing my mind)

---
+++ data/shelldata/init.psm1
@@ -0,0 +1,162 @@
+function InitialiseModules {
+param (
+        [Parameter(Position = 0)]
+        [string]$set,
+        [Parameter(Position = 1)]
+        [string]$module
+    )
+
+    # Expect both or neither, not either or
+    if ([bool]$set -xor [bool]$module) {
+        Write-Host "Missing Var" -ForegroundColor Red
+        return
+    }
+
+    $ini = Import-Ini $global:prefPath
+
+    # Check if paths exist
+    $moduleDir = Join-Path $global:projectDir "modules"
+    if (!(Test-Path $moduleDir)) { New-Item $moduleDir -ItemType Directory | Out-Null }
+    if (!(Test-Path $global:prefPath)) {
+        New-Item $global:prefPath -ItemType File | Out-Null
+        "[Settings]`nprojectDirectory=$global:projectDir`n[Modules]`n" | Out-File -FilePath $global:prefPath -Encoding utf8
+    }
+
+    function _LazyUpdate {
+    param (
+            [string]$action,
+            [string]$mod
+        )
+
+        switch ($action) {
+            "add" {
+                $modPath = Join-Path $moduleDir "$mod.psm1"
+                Set-Item Function:$mod -Value {
+                    Remove-Item Function:$mod -ErrorAction SilentlyContinue
+                    Import-Module $using:modPath
+                    & $mod @PSBoundParameters
+                }
+                Write-Host "$mod added from $modPath" #debug
+            }
+
+            "rm" {
+                if (Test-Path Function:$mod) {
+                    Remove-Item Function:$mod -ErrorAction SilentlyContinue
+                }
+            }
+        }
+    }
+
+    # Change how a single module is loaded
+    if ($set) {
+        $modulePath = Join-Path $moduleDir "$module.psm1"
+
+        switch ($set) {
+            "enable" {
+                Import-Module $modulepath
+                _LazyUpdate rm $module
+                $set = 0
+            }
+
+            "disable" {
+                Remove-Module $module
+                _LazyUpdate rm $module
+                $set = -1
+            }
+
+            "lazy" {
+                Remove-Module $module
+                _LazyUpdate add $module
+                $set = 'lazy'
+
+            }
+
+            { $set -is [int] } {
+                _LazyUpdate rm $module
+
+                if ($set -lt 0) {
+                    Remove-Module $module
+                } else {
+                    Import-Module $module
+                }
+
+                continue
+            }
+        }
+
+        if ($ini['Modules']["$module"] -eq $set) {
+            Write-Host "$module already set to $set" -ForegroundColor Yellow
+            return
+        }
+
+        $ini['Modules']["$module"] = $set
+        Export-Ini -InputObject $ini -Path $global:prefPath -Force
+        Write-Host "$module set to $set" -ForegroundColor Green
+
+        return
+    }
+
+    # Else, reload all modules by reading pref.ini
+    # First remove all modules
+    $prevModules = @()
+    foreach ($mod in $ini['Modules']) {
+        $prevModules += $mod
+        _LazyUpdate rm $mod
+        if (Get-Module -Name $mod) { Remove-Module $mod }
+    }
+
+    # Then, load in every module in the modules directory
+    $modules = Get-ChildItem $moduleDir -File -Filter *.psm1 | ForEach-Object { $_.BaseName }
+
+    $oldModules = @()
+    $newModules = @()
+    if (-not $ini.Contains('Modules')) {
+        $ini['Modules'] = [System.Collections.Specialized.OrderedDictionary]::new()
+    }
+    foreach ($mod in $modules) {
+        if ($ini['Modules'].Contains($mod)) {
+            $flag = $ini['Modules'][$mod]
+        } else {
+            $flag = "uhh it ain't in there brah"
+        }
+
+        # Import every already existing module
+        if ($flag -eq "lazy") {
+            _LazyUpdate add $mod
+            $oldModules += $mod
+        }
+        if ($flag -is [int]) {
+            switch ($flag) {
+                0 {
+                    Import-Module (Join-Path $moduleDir "$mod.psm1")
+                    $oldModules += $mod
+                }
+
+                { $_ -gt 0 } {
+                    Import-Module (Join-Path $moduleDir "$mod.psm1")
+                    & $mod
+                    $oldModules += $mod
+                }
+
+                { $_ -lt 0 } { $oldModules += $mod }
+            }
+        }
+        else {
+            $newModules += $mod
+            $ini['Modules'][$mod] = 'lazy'
+            _LazyUpdate add $mod
+        }
+    }
+    # Add new modules to list
+
+
+    $ini | Export-Ini -Path $global:prefPath -Force
+
+    # write a message for every new and every removed module
+    $addedModules = $newModules | Where-Object { $_ -notin $prevModules }
+    $removedModules = $prevModules | Where-Object { $_ -notin $newModules }
+
+    foreach ($mod in $oldModules) { Write-Host "Loaded '$mod'" -ForegroundColor Yellow }
+    foreach ($mod in $addedModules) { Write-Host "Added '$mod'" -ForegroundColor Green }
+    foreach ($mod in $removedModules) { Write-Host "Removed '$mod'" -ForegroundColor Yellow }
+}
--- shell.ps1
+++ shell.ps1
@@ -1,172 +1,107 @@
-﻿# TODO: 
-#       comment. i'm gonna hate when i get around to it, but i gotta comment this shit
-
-# # window title
-# $host.ui.RawUI.WindowTitle = "Shell"
-
 function shell {
-    param (
+    <#
+        .SYNOPSIS
+            Entrypoint for Shell Module Manager
+
+        .DESCRIPTION
+            Initialises `shell` on startup and auto loads modules based on preferences
+
+        .PARAMETER
+            init    - initialise all modules and environment
+            cd      - go to shell directory
+            vars    - display global variables
+            modules - list loaded modules
+            set     - manage a module
+            pref    - manage preferences
+
+        .PARAMETER altAction
+            init    - cls : clear the console for a freshly loaded environment
+                      (Default) : reload all modules
+            set     - enable / disable / lazy
+                      integers for direct inputs / load order
+            pref    - ii : open pref.ini
+                      (Default) : write preferences to console
+
+        .PARAMETER module
+            A selected module for altAction 'set'
+
+        .NOTES
+            PSIni is a required dependency for writing to pref.ini
+    #>
+param (
         [Parameter(Position = 0)]
         [string]$action,
-
         [Parameter(Position = 1)]
-        [ValidateSet("open", "update", "view", "cd", "dir")]
-        [string]$altAction
+        [string]$altAction,
+        [Parameter(Position = 2)]
+        [string]$module
     )
 
-    function _ShellDirectory {
-        if (Test-Path $global:projectDirectory) {
-            switch ($altAction) {
-                "open" { Invoke-Item $global:projectDirectory }
-
-                "cd" { Set-Location $global:projectDirectory }
-
-                Default { Write-Host "Project Directory:   $($global:projectDirectory)" -ForegroundColor Yellow }
-            }
-        }
-        else {
-            Write-Host "if you see this error, wtf did you do :sob:" -ForegroundColor Red
-            Write-Host "Test-Path `$global:projectDirectory failed" -ForegroundColor Yellow
-        }
-    }
-
-    function _ShellPreferences {
-        switch ($altAction) {
-            "ii" { Invoke-Item $global:prefPath }
-
-            "dir" { 
-                $prefdir = Join-Path $global:prefPath ".." 
-                Invoke-Item $prefdir
-            }
-
-            "cd" {
-                $prefdir = Join-Path $global:prefPath ".."
-                Set-Location $prefdir
-            }
-
-            Default { Get-Content -Path $global:prefPath | ForEach-Object { Write-Host $_ -ForegroundColor Yellow } }
-        }
-    }
-    
     switch ($action) {
-        # Opens project
-        "code" {
-            $global:projectDirectory
-            # If the project directory exists
-            if (!(Test-Path $global:projectDirectory)) {
-                Write-Host "if you see this error, literally how" -ForegroundColor Red
-                Write-Host "Project directory not found" -ForegroundColor Yellow
-                return
-            }
-            
-            # if the user does not have visual studio code / the "code" command
-            if (Get-Command "code" -ErrorAction SilentlyContinue -CommandType Application) {
-                code $global:projectDirectory
-            }
-            else {
-                Write-Host "Visual Studio Code is not installed" -ForegroundColor Red
-                Write-Host "If it is already installed open VSCode and press Ctrl + Shift + P" -ForegroundColor Yellow
-                Write-Host "Run this: Shell Command: install 'code' command in PATH" -ForegroundColor Yellow
-                Write-Host "It should do the rest itself. then you can restart your terminal" -ForegroundColor Yellow
-            }
-        }
-
-        # Opens project
-        "nvim" {
-            # If the project directory exists
-            if (!(Test-Path $global:projectDirectory)) {
-                Write-Host "if you see this error, literally how" -ForegroundColor Red
-                Write-Host "Project directory not found" -ForegroundColor Yellow
+        "init" {
+            # Verify Dependencies
+            if (!(Get-Module -ListAvailable PSIni)) {
+                Write-Host "PSIni not found" -ForegroundColor Red  
+                Write-Host "PSIni is required for 'shell' to work" -ForegroundColor Yellow  
+                Write-Host "it can be installed with the following command:" -ForegroundColor Yellow  
+                Write-Host "Install-Module PSIni -Scope CurrentUser -Force" -ForegroundColor Cyan  
                 return
             }
 
-            # if the user does not have neovim
-            if (Get-Command "nvim" -ErrorAction SilentlyContinue) {
-                nvim $global:projectDirectory
+            if ($altAction -eq "cls") { Clear-Host }
+
+            Import-Module (Join-Path $global:projectDir ".\data\shelldata\init.psm1")
+            InitialiseModules
+            Remove-Module init
+        }
+
+        "cd" { Set-Location $global:projectDir }
+
+        "vars" {
+            Write-Host "`$global:projectDir : $global:projectDir" -ForegroundColor Yellow
+            Write-Host "`$global:prefPath : $global:prefPath" -ForegroundColor Yellow
+        }
+
+        "mods" {
+            $moduleDir = Join-Path $global:projectDir "$modules"
+            Import-Module (Join-Path $global:projectDir ".\data\shelldata\init.psm1")
+
+            if (!(Test-Path $moduleDir)) {
+                Write-Host "Module directory not found" -ForegroundColor Red
+                return
             }
-            else {
-                Write-Host "Neovim is not installed" -ForegroundColor Red
+
+            switch ($altAction) {
+                "enable" { InitialiseModules $altAction $module }
+                "disable" { InitialiseModules $altAction $module }
+                "lazy" { InitialiseModules $altAction $module }
+                { $altAction -is [int] } { InitialiseModules $altAction $module }
+                Default {
+                    Write-Host "forgot to write this" -ForegroundColor Yellow
+                }
             }
         }
 
-        "dir" { _ShellDirectory }
-        
-        "directory" { _ShellDirectory }
+        "pref" {
+            switch ($altAction) {
+                "ii" { Invoke-Item $global:prefPath }
 
-        # Display all global variables
-        "globals" {
-            Write-Host "prefPath:                   $global:prefPath" -ForegroundColor Yellow
-            Write-Host "pref:                       $global:pref" -ForegroundColor Yellow
-            Write-Host "projectDirectory:           $global:projectDirectory" -ForegroundColor Yellow
-        }
-
-        "preferences" { _ShellPreferences }
-
-        "pref" { _ShellPreferences }
-
-        # Check for missing dependencies and ask the user if they want to install them
-        "dependencies" {
-            # List of currently required dependencies
-            $dependencies = @(
-                "PsIni"
-                # add node.js
-            )
-            $missingModuleCount = 0
-            $missingModules = @()
-            $response = "N"
-            
-            # Check if any dependencies in the list are missing
-            foreach ($module in $dependencies) {
-                if (-not (Get-Module -ListAvailable -Name $module)) {
-                    $missingModuleCount ++
-                    $missingModules += $module
-                    Write-Host "Missing dependency: $module" -ForegroundColor Red
-                }
-                else {
-                    Write-Host "$module is present" -ForegroundColor Yellow
-                }
-            }
-
-            # If dependencies are missing, inform user and ask if they want to install them
-            if ($missingModuleCount -eq 0) {
-                Write-Host "No missing dependencies found" -ForegroundColor Green
-            }
-            else {
-                Write-Host "$missingModuleCount missing dependencies found" -ForegroundColor Red
-                Write-Host "Install them now? [Y]/[N]" -ForegroundColor Yellow
-                $response = Read-Host
-            }
-
-            if ($response -match "^[Yy]") {
-                foreach ($module in $missingModules) {
-                    try {
-                        Install-Module -Name $module -Force -Scope CurrentUser
-                        Write-Host "$module installed successfully" -ForegroundColor Green
-                    }
-                    catch {
-                        Write-Host "$module failed to install" -ForegroundColor Red
-                    }
+                # Write preferences to console
+                Default {
+                    Get-Content $global:prefPath
                 }
             }
         }
 
         Default {
-            Write-Host "Command not found" -ForegroundColor Red
+            Write-Host "psmm is working yippee" -ForegroundColor Green
         }
     }
 }
 
-# Startup
-$global:projectDirectory = $PSScriptRoot
-$global:prefPath = Join-Path $PSScriptRoot "data\pref.ini"
-$shellDataPath = Join-Path $PSScriptRoot "modules\shelldata"
-$initPath = Join-Path $shellDataPath "init.psm1"
-if (Test-Path $initPath) {
-    Import-Module $initPath -Force
-    initialise
-}
-else {
-    Write-Host "Failed to initialise shell" -ForegroundColor Red
-    Write-Host "init.psm1 not found in \modules\shelldata" -ForegroundColor Yellow
-}
 
+
+# Initialisation
+$global:projectDir = $PSScriptRoot
+$global:prefPath = Join-Path $global:projectDir "data\pref.ini"
+shell init
